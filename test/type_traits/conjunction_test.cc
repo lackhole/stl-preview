f34852a0281a961d40077c3691472c45f6aa7142
Yong Gyu Lee <cosgenio@gmail.com>
2024-06-15 20:39:49 +0900

    1

--- test/type_traits/conjunction_test.cc
+++ test/type_traits/conjunction_test.cc
@@ -6,10 +6,18 @@
 
 #include <type_traits>
 
-#include <gtest/gtest.h>
+#include "../gtest.h"
 
+template<typename T>
+struct NumberOnly {
+  static constexpr int value = T{};
+};
 
-TEST(HelloTest, BasicAssertions) {
+TEST(VERSIONED(conjunction), basics) {
   EXPECT_TRUE((preview::conjunction_v<std::is_integral<int>, std::is_floating_point<float>>));
   EXPECT_FALSE((preview::conjunction_v<std::is_integral<int>, std::is_floating_point<int>>));
+}
+
+TEST(VERSIONED(conjunction), instantiate) {
+  EXPECT_TRUE((preview::conjunction_v<std::is_integral<float>, NumberOnly<void>>));
 }
