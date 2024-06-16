40432a90853b11d1f93b4c72860ee5d019e5140b
Yong Gyu Lee <cosgenio@gmail.com>
2024-06-17 02:45:45 +0900

    Update optional

--- include/preview/__optional/hash.h
+++ include/preview/__optional/hash.h
@@ -10,19 +10,47 @@
 # include <functional>
 # include <type_traits>
 #
-# include "preview/__optional/internal/hash.h"
 # include "preview/__optional/optional.h"
+
+namespace preview {
+namespace detail {
+
+template<typename Optional>
+struct optional_enabled_hash;
+
+template<typename T>
+struct optional_enabled_hash<optional<T>> {
+  std::size_t operator()(const optional<T>& key) const noexcept {
+    return key.has_value() ? std::hash<std::remove_const_t<T>>()(*key) : 0;
+  }
+};
+
+struct optional_disabled_hash {
+  optional_disabled_hash() = delete;
+};
+
+template<typename Optional>
+struct optional_hash;
+
+template<typename T>
+struct optional_hash<optional<T>>
+    : std::conditional_t<
+        std::is_default_constructible<std::hash<T>>::value,
+        optional_enabled_hash<optional<T>>,
+        optional_disabled_hash
+    > {};
+
+} // namespace detail
+} // namespace preview
 
 namespace std {
 
 template<typename T>
-struct hash<preview::optional<T>> : preview::internal::optional::hash_constructible<std::remove_const_t<T>> {
+struct hash<preview::optional<T>>
+    : preview::detail::optional_hash<preview::optional<T>>
+{
   using argument_type = preview::optional<T>;
   using result_type = std::size_t;
-
-  result_type operator()(const argument_type& key) const {
-    return key.has_value() ? ::std::hash<std::remove_const_t<T>>()(*key) : 0;
-  }
 };
 
 } // namespace std
