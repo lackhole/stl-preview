2f2e421063bb6064fa7bf5defda788e693d6e47a
Yong Gyu Lee <cosgenio@gmail.com>
2024-06-19 09:20:49 +0900

    Add flag

--- test/gtest.h
+++ test/gtest.h
@@ -7,11 +7,16 @@
 
 #include <gtest/gtest.h>
 
+#include <type_traits>
+
 #include "preview/__core/std_version.h"
 
 #define VERSION_CAT2(x, y) x ## _CXXSTD_ ## y
 #define VERSION_CAT(x, y) VERSION_CAT2(x, y)
 
 #define VERSIONED(name) VERSION_CAT(name, PREVIEW_CXX_VERSION)
+
+#define EXPECT_EQ_TYPE(type1, type2) \
+    EXPECT_TRUE((std::is_same<type1, type2>::value))
 
 #endif //PREVIEW_TEST_GTEST_H_
