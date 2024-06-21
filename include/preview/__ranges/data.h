63fefbd6389643c6c38c9a9a85f7124acfdd17f5
Yong Gyu Lee <cosgenio@gmail.com>
2024-06-22 00:57:18 +0900

    Fix ranges::data

--- include/preview/__ranges/data.h
+++ include/preview/__ranges/data.h
@@ -11,8 +11,9 @@
 #include "preview/__core/decay_copy.h"
 #include "preview/__iterator/contiguous_iterator.h"
 #include "preview/__memory/to_address.h"
+#include "preview/__ranges/detail/not_incomplete_array.h"
 #include "preview/__ranges/begin.h"
-#include "preview/__type_traits/detail/return_category.h"
+#include "preview/__ranges/range_reference_t.h"
 #include "preview/__type_traits/disjunction.h"
 #include "preview/__type_traits/void_t.h"
 
@@ -20,66 +21,77 @@ namespace preview {
 namespace ranges {
 namespace detail {
 
-using preview::detail::return_category;
-
-template<typename P>
-struct data_member_check_2 : std::false_type {
-  using category = return_category<0>;
-};
-template<typename P>
-struct data_member_check_2<P*> : std::is_object<P> {
-  using category = return_category<1, P*>;
-};
-
-template<typename T, typename = void>
-struct data_member_check : std::false_type {
-  using category = return_category<0>;
-};
-template<typename T>
-struct data_member_check<T, void_t<decltype( preview_decay_copy(std::declval<T>().data()) )>>
-    : data_member_check_2<decltype( preview_decay_copy(std::declval<T>().data()) )> {};
-
-
-template<typename T, typename = void>
-struct data_ranges_begin_check : std::false_type {
-  using category = return_category<0>;
-};
-template<typename T>
-struct data_ranges_begin_check<T, void_t<decltype( ranges::begin(std::declval<T>()) )>>
-    : contiguous_iterator<decltype( ranges::begin(std::declval<T>()) )>
-{
-  using category = return_category<2, decltype( preview::to_address( ranges::begin(std::declval<T>()) ) )>;
-};
-
-template<typename T>
-struct data_category
-    : std::conditional_t<
-          data_member_check<T>::value, typename data_member_check<T>::category,
-      std::conditional_t<
-          data_ranges_begin_check<T>::value, typename data_ranges_begin_check<T>::category,
-          return_category<0>
-      >>{};
-
 struct data_niebloid {
-  template<typename T, std::enable_if_t<disjunction<
-      std::is_lvalue_reference<T&&>,
-      ranges::enable_borrowed_range<remove_cvref_t<T>>
-  >::value, int> = 0>
-  constexpr typename data_category<T&&>::return_type
-  operator()(T&& t) const {
-    return (*this)(std::forward<T>(t), data_category<T&&>{});
-  }
-
  private:
-  template<typename T, typename R>
-  constexpr R operator()(T&& t, return_category<1, R>) const {
+  template<typename T>
+  struct is_pointer_to_object : std::false_type {};
+  template<typename T>
+  struct is_pointer_to_object<T*> : std::is_object<T*> {};
+
+  template<typename T, typename = void>
+  struct member_function_data_is_pointer_to_object
+      : std::false_type {};
+  template<typename T>
+  struct member_function_data_is_pointer_to_object<T, void_t<decltype( preview_decay_copy(std::declval<T>().data()) )>>
+      : conjunction<
+          is_pointer_to_object<decltype( preview_decay_copy(std::declval<T>().data()) )>,
+          std::is_convertible<
+              decltype( preview_decay_copy(std::declval<T>().data()) ),
+              std::remove_reference_t<ranges::range_reference_t<T>>*
+          >
+      > {};
+
+  template<typename T, typename = void>
+  struct ranges_begin_is_contiguous_iterator
+      : std::false_type {};
+  template<typename T>
+  struct ranges_begin_is_contiguous_iterator<T, void_t<decltype( ranges::begin(std::declval<T>()) )>>
+      : conjunction<
+          contiguous_iterator<decltype( ranges::begin(std::declval<T>()) )>,
+          std::is_convertible<
+              decltype( ranges::begin(std::declval<T>()) ),
+              std::remove_reference_t<ranges::range_reference_t<T>>*
+          >
+      > {};
+
+  template<typename T>
+  constexpr std::remove_reference_t<ranges::range_reference_t<T>>*
+  call(T&& t, std::true_type) const {
     return preview_decay_copy(t.data());
   }
 
-  template<typename T, typename R>
-  constexpr R operator()(T&& t, return_category<2, R>) const {
+  template<typename T>
+  constexpr std::remove_reference_t<ranges::range_reference_t<T>>*
+  call(T&& t, std::false_type) const {
     return preview::to_address(ranges::begin(t));
   }
+
+ public:
+  template<typename T, std::enable_if_t<conjunction<
+      preview::detail::not_incomplete_array<T>,
+      disjunction<
+          std::is_lvalue_reference<T&&>,
+          ranges::enable_borrowed_range<remove_cvref_t<T>>
+      >,
+      has_typename_type<range_reference<T>>,
+      disjunction<
+          member_function_data_is_pointer_to_object<T&&>,
+          ranges_begin_is_contiguous_iterator<T&&>
+      >
+  >::value, int> = 0>
+  constexpr std::remove_reference_t<ranges::range_reference_t<T>>*
+  operator()(T&& t) const {
+    return call(std::forward<T>(t), member_function_data_is_pointer_to_object<T&&>{});
+  }
+
+#if PREVIEW_CXX_VERSION < 17
+  // non-const version of std::basic_string::data was introduced since C++17
+
+  template<typename CharT, typename Traits, typename Allocator>
+  constexpr CharT* operator()(std::basic_string<CharT, Traits, Allocator>& str) const {
+    return const_cast<CharT*>(str.data());
+  }
+#endif
 };
 
 } // namespace detail
