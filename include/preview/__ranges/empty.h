07b62ed411dfdee61476c4dd08c93e468d050b02
Yong Gyu Lee <cosgenio@gmail.com>
2024-07-09 01:56:09 +0900

    Refactor ranges::empty

--- include/preview/__ranges/empty.h
+++ include/preview/__ranges/empty.h
@@ -12,7 +12,7 @@
 #include "preview/__ranges/begin.h"
 #include "preview/__ranges/end.h"
 #include "preview/__ranges/size.h"
-#include "preview/__type_traits/detail/return_category.h"
+#include "preview/__type_traits/conditional.h"
 #include "preview/__type_traits/void_t.h"
 #include "preview/__utility/cxx20_rel_ops.h"
 
@@ -20,83 +20,63 @@ namespace preview {
 namespace ranges {
 namespace detail {
 
-using preview::detail::return_category;
-
-template<typename T, typename = void>
-struct empty_member_check : std::false_type {
-  using category = return_category<0>;
-};
-template<typename T>
-struct empty_member_check<T, void_t<decltype( bool(std::declval<T>().empty()) )>> : std::true_type {
-  using category = return_category<1, bool>;
-};
-
-template<typename T, typename = void>
-struct empty_size_check : std::false_type {
-  using category = return_category<0>;
-};
-template<typename T>
-struct empty_size_check<T, void_t<decltype( ranges::size(std::declval<T>()) == 0 )>> : std::true_type {
-  using category = return_category<2, bool>;
-};
-
-
-template<typename I, typename S, bool = rel_ops::is_equality_comparable<I, S>()>
-struct empty_range_check_2 : std::false_type {
-  using category = return_category<0>;
-};
 template<typename I, typename S>
-struct empty_range_check_2<I, S, true> : forward_iterator<I> {
-  using category = return_category<3, bool>;
-};
-
-template<typename R, typename I = void, typename S = void>
-struct empty_range_check : std::false_type {
-  using category = return_category<0>;
-};
-template<typename R>
-struct empty_range_check<
-      R,
-      void_t<decltype( ranges::begin(std::declval<R>()) )>,
-      void_t<decltype( ranges::end  (std::declval<R>()) )>
-    >
-    : empty_range_check_2<
-        /* I = */decltype( ranges::begin(std::declval<R>()) ),
-        /* S = */decltype( ranges::end  (std::declval<R>()) )
-      > {};
-
-template<typename T>
-struct empty_return_category
-    : std::conditional_t<
-        empty_member_check<T>::value, typename empty_member_check<T>::category,
-      std::conditional_t<
-        empty_size_check<T>::value, typename empty_size_check<T>::category,
-      std::conditional_t<
-        empty_range_check<T>::value, typename empty_range_check<T>::category,
-        return_category<0>
-      >>> {};
-
-template<typename T, typename R>
-constexpr R empty_impl(T&& t, return_category<1, R>) {
-  return bool(t.empty());
-}
-
-template<typename T, typename R>
-constexpr R empty_impl(T&& t, return_category<2, R>) {
-  return (ranges::size(t) == 0);
-}
-
-template<typename T, typename R>
-constexpr R empty_impl(T&& t, return_category<3, R>) {
-  using namespace preview::rel_ops;
-  return bool(ranges::begin(t) == ranges::end(t));
-}
+struct begin_end_comparable_range_impl
+  : conjunction<
+    rel_ops::is_equality_comparable<I, S>,
+    forward_iterator<I>
+  > {};
 
 struct empty_niebloid {
+ private:
+  template<typename T, typename = void>
+  struct has_member_empty : std::false_type {};
   template<typename T>
-  constexpr typename empty_return_category<T&&>::return_type
-  operator()(T&& t) const {
-    return detail::empty_impl(std::forward<T>(t), detail::empty_return_category<T&&>{});
+  struct has_member_empty<T, void_t<decltype( bool(std::declval<T>().empty()) )>> : std::true_type {};
+
+  template<typename T, typename = void>
+  struct has_ranges_size : std::false_type {};
+  template<typename T>
+  struct has_ranges_size<T, void_t<decltype( ranges::size(std::declval<T>()) == 0 )>> : std::true_type {};
+
+  template<typename R, bool = range<R>::value /* false */>
+  struct begin_end_comparable_range : std::false_type {};
+  template<typename R>
+  struct begin_end_comparable_range<R, true>
+    : begin_end_comparable_range_impl<
+      /* I = */decltype( ranges::begin(std::declval<R>()) ),
+      /* S = */decltype( ranges::end  (std::declval<R>()) )
+    > {};
+
+  template<typename T>
+  constexpr bool call(T&& t, std::integral_constant<int, 0>) const {
+    return bool(t.empty());
+  }
+
+  template<typename T>
+  constexpr bool call(T&& t, std::integral_constant<int, 1>) const {
+    return (ranges::size(t) == 0);
+  }
+
+  template<typename T>
+  constexpr bool call(T&& t, std::integral_constant<int, 2>) const {
+    using namespace preview::rel_ops;
+    return bool(ranges::begin(t) == ranges::end(t));
+  }
+
+ public:
+  template<typename T, std::enable_if_t<disjunction<
+      has_member_empty<T>,
+      has_ranges_size<T>,
+      begin_end_comparable_range<T>
+  >::value, int> = 0>
+  constexpr bool operator()(T&& t) const {
+    using tag = conditional_t<
+        has_member_empty<T>,          std::integral_constant<int, 0>,
+        has_ranges_size<T>,           std::integral_constant<int, 1>,
+        /* begin_end_comparable<T> */ std::integral_constant<int, 2>
+    >;
+    return call(std::forward<T>(t), tag{});
   }
 };
 
