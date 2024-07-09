0727b0541a7c512ca9a3182046105604e1effd29
Yong Gyu Lee <cosgenio@gmail.com>
2024-07-09 22:05:47 +0900

    Create unwrap_reference.h

---
+++ include/preview/__type_traits/unwrap_reference.h
@@ -0,0 +1,34 @@
+//
+// Created by YongGyu Lee on 2024. 7. 9.
+//
+
+#ifndef PREVIEW_TYPE_TRAITS_UNWRAP_REFERENCE_H_
+#define PREVIEW_TYPE_TRAITS_UNWRAP_REFERENCE_H_
+
+#include <functional>
+#include <type_traits>
+
+namespace preview {
+
+template<typename T>
+struct unwrap_reference {
+  using type = T;
+};
+
+template<typename T>
+struct unwrap_reference<std::reference_wrapper<T>> {
+  using type = T&;
+};
+
+template<typename T>
+using unwrap_reference_t = typename unwrap_reference<T>::type;
+
+template<typename T>
+struct unwrap_ref_decay : unwrap_reference<std::decay_t<T>> {};
+
+template<typename T>
+using unwrap_ref_decay_t = typename unwrap_ref_decay<T>::type;
+
+} // namespace preview
+
+#endif // PREVIEW_TYPE_TRAITS_UNWRAP_REFERENCE_H_
