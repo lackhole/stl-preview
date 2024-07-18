6c0799ee051689ac2f2810bc91f2476dcec2477c
Yong Gyu Lee <cosgenio@gmail.com>
2024-07-19 00:29:36 +0900

    Init files

---
+++ include/preview/__atomic/atomic_wait.h
@@ -0,0 +1,12 @@
+//
+// Created by yonggyulee on 2024. 7. 19.
+//
+
+#ifndef PREVIEW_ATOMIC_ATOMIC_WAIT_H_
+#define PREVIEW_ATOMIC_ATOMIC_WAIT_H_
+
+namespace preview {
+
+} // namespace preview
+
+#endif // PREVIEW_ATOMIC_ATOMIC_WAIT_H_
