29f8577dff22351516dd8545e3c813061258dba6
Yong Gyu Lee <cosgenio@gmail.com>
2024-07-19 02:44:23 +0900

    Implement individual configuration for each tests

--- include/preview/__iterator/iterator_tag.h
+++ include/preview/__iterator/iterator_tag.h
@@ -7,6 +7,8 @@
 
 #include <iterator>
 
+#include "preview/config.h"
+
 namespace preview {
 
 using input_iterator_tag = std::input_iterator_tag;
@@ -15,10 +17,10 @@ using forward_iterator_tag = std::forward_iterator_tag;
 using bidirectional_iterator_tag = std::bidirectional_iterator_tag;
 using random_access_iterator_tag = std::random_access_iterator_tag;
 
-#if __cplusplus < 202002L
-struct contiguous_iterator_tag : public random_access_iterator_tag {};
-#else
+#if PREVIEW_HAVE_CONTIGUOUS_ITERATOR_TAG
 using contiguous_iterator_tag = std::contiguous_iterator_tag;
+#else
+struct contiguous_iterator_tag : public random_access_iterator_tag {};
 #endif
 
 // placeholder for ignoring typedefs
