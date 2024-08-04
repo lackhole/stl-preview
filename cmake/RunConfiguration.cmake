546b6c7f88838737115a2eeb80df7f683d6db153
Yong Gyu Lee <cosgenio@gmail.com>
2024-08-04 18:21:54 +0900

    Add tests for existing implementations

--- cmake/RunConfiguration.cmake
+++ cmake/RunConfiguration.cmake
@@ -3,7 +3,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSymbolExists)
 
 include(config/HaveBuiltinBitCast)
-include(config/HaveConstexprBuiltinAddressof)
+include(config/HaveBuiltinConstexprAddressof)
 include(config/HaveContiguousIteratorTag)
 include(config/HaveStringView)
 
@@ -11,7 +11,8 @@ set(PREVIEW_CONFIG_INPUT_FILE "${PREVIEW_INCLUDE_DIR}/preview/cmake_config.h.in"
 
 function(RunConfiguration out_dir flags)
     HaveBuiltinBitCast(PREVIEW_HAVE_BUILTIN_BIT_CAST ${flags})
-    HaveConstexprBuiltinAddressof(PREVIEW_HAVE_CONSTEXPR_BUILTIN_ADDRESSOF ${flags})
+    HaveBuiltinConstexprAddressof(PREVIEW_HAVE_BUILTIN_CONSTEXPR_ADDRESSOF ${flags})
+
     HaveContiguousIteratorTag(PREVIEW_HAVE_CONTIGUOUS_ITERATOR_TAG ${flags})
     HaveStringView(PREVIEW_HAVE_STRING_VIEW ${flags})
 
