a284371e48401744286d30c1a2adbfe8bfac9adc
Yong Gyu Lee <cosgenio@gmail.com>
2024-08-04 16:34:04 +0900

    Implement bit_cast (#34)
    * Implement bit_cast
    
    * Update README.md

--- cmake/RunConfiguration.cmake
+++ cmake/RunConfiguration.cmake
@@ -2,12 +2,14 @@ include(CheckCXXSourceCompiles)
 include(CheckCXXCompilerFlag)
 include(CheckCXXSymbolExists)
 
+include(config/HaveBuiltinBitCast)
 include(config/HaveContiguousIteratorTag)
 include(config/HaveStringView)
 
 set(PREVIEW_CONFIG_INPUT_FILE "${PREVIEW_INCLUDE_DIR}/preview/cmake_config.h.in")
 
 function(RunConfiguration out_dir flags)
+    HaveBuiltinBitCast(PREVIEW_HAVE_BUILTIN_BIT_CAST ${flags})
     HaveContiguousIteratorTag(PREVIEW_HAVE_CONTIGUOUS_ITERATOR_TAG ${flags})
     HaveStringView(PREVIEW_HAVE_STRING_VIEW ${flags})
 
