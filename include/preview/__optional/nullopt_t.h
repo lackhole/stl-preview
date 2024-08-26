cb7b88bff2e375e53bb6a9ecf0dadc03a5ff6e68
Yong Gyu Lee <cosgenio@gmail.com>
2024-08-26 19:26:10 +0900

    Use std::nullopt if possible

--- include/preview/__optional/nullopt_t.h
+++ include/preview/__optional/nullopt_t.h
@@ -5,13 +5,30 @@
 # ifndef PREVIEW_OPTIONAL_NULLOPT_T_H_
 # define PREVIEW_OPTIONAL_NULLOPT_T_H_
 
+#include "preview/__core/std_version.h"
+
+#if PREVIEW_CXX_VERSION >= 17
+#include <optional>
+#endif
+
 namespace preview {
 
+#if PREVIEW_CXX_VERSION >= 17
+
+using nullopt_t = std::nullopt_t;
+constexpr nullopt_t nullopt{std::nullopt};
+
+#else
+
 struct nullopt_t {
-  constexpr explicit nullopt_t(int) {}
+  struct ctor_tag {};
+  constexpr explicit nullopt_t(ctor_tag, ctor_tag) {}
 };
 
-constexpr nullopt_t nullopt{0};
+constexpr nullopt_t nullopt{nullopt_t::ctor_tag{}, nullopt_t::ctor_tag{}};
+
+#endif
+
 
 } // namespace preview
 
