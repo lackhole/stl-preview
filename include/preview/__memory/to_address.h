1738a35cff779d6f536f05b6e38cee33c7d01e47
Yong Gyu Lee <cosgenio@gmail.com>
2024-08-26 21:20:47 +0900

    Update compatibility with `std::` (#42)
    * Use std::nullopt if possible
    
    * Use std::pointer_traits::to_address if possible
    
    * Update README.md
    
    * Fix dumb MSVC bug

--- include/preview/__memory/to_address.h
+++ include/preview/__memory/to_address.h
@@ -9,24 +9,39 @@
 #include <type_traits>
 
 #include "preview/__memory/pointer_traits.h"
+#include "preview/__type_traits/detail/tag.h"
 #include "preview/__type_traits/has_operator_arrow.h"
 #include "preview/__type_traits/void_t.h"
 
 namespace preview {
 namespace detail {
 
-template<typename T, typename = void>
+template<typename T, template<typename> class Traits, typename = void>
 struct has_to_address : std::false_type {};
 
-template<typename T>
-struct has_to_address<T, void_t<decltype(pointer_traits<T>::to_address(std::declval<const T&>()))>> : std::true_type {};
+template<typename T, template<typename> class Traits>
+struct has_to_address<T, Traits, void_t<decltype(Traits<T>::to_address(std::declval<const T&>()))>> : std::true_type {};
 
 template<typename T>
-constexpr auto to_address_fancy(const T& p, std::true_type /* has_to_address */ ) noexcept {
-  return preview::pointer_traits<T>::to_address(p);
+using to_address_tag = conditional_tag<
+#if defined(_MSC_VER) && _MSC_VER < 1930 // std::pointer_traits is not sfinae-friendly before VS 2022
+    has_to_address<T, pointer_traits>
+#else
+    has_to_address<T, std::pointer_traits>,
+    has_to_address<T, pointer_traits>
+#endif
+>;
+
+template<typename T>
+constexpr auto to_address_fancy(const T& p, preview::detail::tag_1 /* std::pointer_traits<T>::to_address */ ) noexcept {
+  return std::pointer_traits<T>::to_address(p);
 }
 template<typename T>
-constexpr auto to_address_fancy(const T& p, std::false_type /* has_to_address */ ) noexcept;
+constexpr auto to_address_fancy(const T& p, preview::detail::tag_2 /* pointer_traits<T>::to_address */ ) noexcept {
+  return pointer_traits<T>::to_address(p);
+}
+template<typename T>
+constexpr auto to_address_fancy(const T& p, preview::detail::tag_else /* no to_address */ ) noexcept;
 
 } // namespace detail
 
@@ -38,13 +53,13 @@ constexpr T* to_address(T* p) noexcept {
 
 template<class T>
 constexpr auto to_address(const T& p) noexcept {
-  return detail::to_address_fancy(p, detail::has_to_address<T>{});
+  return detail::to_address_fancy(p, detail::to_address_tag<T>{});
 }
 
 namespace detail {
 
 template<typename T>
-constexpr auto to_address_fancy(const T& p, std::false_type /* has_to_address */ ) noexcept {
+constexpr auto to_address_fancy(const T& p, preview::detail::tag_else /* no to_address */ ) noexcept {
   return preview::to_address(p.operator->());
 }
 
