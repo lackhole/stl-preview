919a1e3654282c18b72347f5c452105b47e28fd8
Yong Gyu Lee <cosgenio@gmail.com>
2024-08-29 22:03:22 +0900

    Update iterator.cc

--- test/iterator.cc
+++ test/iterator.cc
@@ -1,5 +1,17 @@
 #include "preview/iterator.h"
 #include "gtest.h"
 
-// TODO: Write test
-TEST(VERSIONED(Iterator), basics) {}
+
+template<class T>
+struct Holder {
+  T t;
+};
+
+struct Incomplete;
+using P = Holder<Incomplete>*;
+
+TEST(VERSIONED(Iterator), projected) {
+  EXPECT_TRUE_TYPE(preview::equality_comparable<P>); // OK
+  EXPECT_TRUE_TYPE(preview::indirectly_comparable<P*, P*, std::equal_to<>>); // Error before C++26
+  EXPECT_TRUE_TYPE(preview::sortable<P*>); // Error before C++26
+}
