baf9923ea62443233bd546ea0b5c90262071a934
Yong Gyu Lee <cosgenio@gmail.com>
2024-09-07 01:25:40 +0900

    Update concepts_v2.h

--- include/preview/concepts_v2.h
+++ include/preview/concepts_v2.h
@@ -5,6 +5,8 @@
 #ifndef PREVIEW_CONCEPTS_V2_H_
 #define PREVIEW_CONCEPTS_V2_H_
 
+#include "preview/__concepts_v2/constructible_from.h"
+#include "preview/__concepts_v2/destructible.h"
 #include "preview/__concepts_v2/floating_point.h"
 #include "preview/__concepts_v2/integral.h"
 #include "preview/__concepts_v2/require.h"
