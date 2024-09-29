6cb86a26a8846ebef3a5778c9c8199899d31951b
Yong Gyu Lee <cosgenio@gmail.com>
2024-09-29 11:31:57 +0900

    Update integer_sequence.h

--- include/preview/__utility/integer_sequence.h
+++ include/preview/__utility/integer_sequence.h
@@ -23,6 +23,36 @@ struct integer_sequence_ranged {
   using type = typename integer_sequence_op_add<T, base_seq, Begin>::type;
 };
 
+template<typename Seq, std::size_t N, typename Out = std::integer_sequence<typename Seq::value_type>>
+struct integer_sequence_slice_left_impl;
+
+template<typename T, T ...I, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I...>, 0, std::integer_sequence<T, J...>> {
+  using type = std::integer_sequence<T, J...>;
+};
+template<typename T, T I0, T ...Is, std::size_t N, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I0, Is...>, N, std::integer_sequence<T, J...>> {
+  using type = typename integer_sequence_slice_left_impl<std::integer_sequence<T, Is...>, N - 1, std::integer_sequence<T, J..., I0>>::type;
+};
+
+// Specialize for small values to get faster compilation
+template<typename T, T I0, T ...Is, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I0, Is...>, 1, std::integer_sequence<T, J...>> {
+  using type = std::integer_sequence<T, J..., I0>;
+};
+template<typename T, T I0, T I1, T ...Is, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I0, I1, Is...>, 2, std::integer_sequence<T, J...>> {
+  using type = std::integer_sequence<T, J..., I0, I1>;
+};
+template<typename T, T I0, T I1, T I2, T ...Is, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I0, I1, I2, Is...>, 3, std::integer_sequence<T, J...>> {
+  using type = std::integer_sequence<T, J..., I0, I1, I2>;
+};
+template<typename T, T I0, T I1, T I2, T I3, T ...Is, T ...J>
+struct integer_sequence_slice_left_impl<std::integer_sequence<T, I0, I1, I2, I3, Is...>, 4, std::integer_sequence<T, J...>> {
+  using type = std::integer_sequence<T, J..., I0, I1, I2, I3>;
+};
+
 } // namespace detail
 
 template<typename T, T Begin, T Bound>
@@ -30,6 +60,23 @@ using make_integer_sequence = typename detail::integer_sequence_ranged<T, Begin,
 
 template<std::size_t Begin, std::size_t End>
 using make_index_sequence = make_integer_sequence<std::size_t, Begin, End>;
+
+
+// integer_sequence_count
+template<typename Seq, typename Seq::value_type X>
+struct integer_sequence_count;
+
+template<typename T, T X>
+struct integer_sequence_count<std::integer_sequence<T>, X>
+    : std::integral_constant<std::size_t, 0> {};
+
+template<typename T, T I0, T ...Is, T X>
+struct integer_sequence_count<std::integer_sequence<T, I0, Is...>, X>
+    : std::integral_constant<std::size_t, (I0 == X) + integer_sequence_count<std::integer_sequence<T, Is...>, X>::value> {};
+
+// alias of integer_sequence<T, Ti...> where i is [0, N) (N < original-length)
+template<typename Seq, std::size_t N>
+using integer_sequence_slice_left = typename detail::integer_sequence_slice_left_impl<Seq, N>::type;
 
 } // namespace preview
 
