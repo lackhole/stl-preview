46fcf424a0a300c63f6867cd5a504668fb643b9d
Yong Gyu Lee <cosgenio@gmail.com>
2024-09-29 11:32:15 +0900

    Init

---
+++ include/preview/__mdspan/submdspan_mapping_result.h
@@ -0,0 +1,12 @@
+//
+// Created by yonggyulee on 2024. 9. 27.
+//
+
+#ifndef PREVIEW_MDSPAN_SUBMDSPAN_SLICE_RESULT_H_
+#define PREVIEW_MDSPAN_SUBMDSPAN_SLICE_RESULT_H_
+
+namespace preview {
+
+} // namespace preview
+
+#endif // PREVIEW_MDSPAN_SUBMDSPAN_SLICE_RESULT_H_
