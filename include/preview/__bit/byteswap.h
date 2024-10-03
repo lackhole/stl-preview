9340d0ee6d527f8479e7125055ee5086cf6774fd
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2024-10-04 00:32:32 +0200

    Added byteswap

---
+++ include/preview/__bit/byteswap.h
@@ -0,0 +1,56 @@
+//
+// Created by Chi-Iroh on 03/10/2024.
+//
+
+#ifndef PREVIEW_BIT_BYTESWAP_H_
+#define PREVIEW_BIT_BYTESWAP_H_
+
+#include <climits>
+#include <cstdint>
+#include <type_traits>
+
+#include "preview/config.h"
+#include "preview/__bit/bit_cast.h"
+#include "preview/__type_traits/negation.h"
+
+#if PREVIEW_CXX_VERSION >= 23
+    #include <bit>
+#endif
+
+namespace preview {
+    namespace detail {
+        template<typename T, std::enable_if_t<std::is_unsigned<T>::value, int> = 0>
+        constexpr T byteswap_impl(T n) noexcept {
+            constexpr T byte_mask{ (1 << CHAR_BIT) - 1 };
+
+            T result{ 0 };
+            for (unsigned i{ 0 }; i < sizeof(T); i++) {
+                const std::size_t nth_byte{ CHAR_BIT * (sizeof(T) - i - 1) };
+                const T byte{ static_cast<T>((n & (byte_mask << nth_byte)) >> nth_byte) };
+                result |= byte << (i * CHAR_BIT);
+            }
+            return result;
+        }
+
+        template<typename T, std::enable_if_t<std::is_signed<T>::value, int> = 0, typename UnsignedT = typename std::make_unsigned<T>::type>
+        constexpr UnsignedT byteswap_impl(T n) {
+            return byteswap_impl<UnsignedT>(preview::bit_cast<UnsignedT>(n));
+        }
+
+        template<typename T, std::enable_if_t<preview::negation_v<std::is_integral<T>>, int> = 0>
+        constexpr T byteswap_impl(T) {
+            static_assert(false, "byteswap needs integral type !");
+        }
+    }
+
+    template<typename T>
+    constexpr T byteswap(T n) noexcept {
+#if PREVIEW_CXX_VERSION >= 23
+    return std::byteswap<T>(n);
+#else
+    return preview::detail::byteswap_impl<T>(n);
+#endif
+    }
+}
+
+#endif
--- test/bit.cc
+++ test/bit.cc
@@ -20,3 +20,43 @@ TEST(VERSIONED(bit), bit_cast) {
   auto f64v2 = preview::bit_cast<double>(u64v2);
   EXPECT_EQ(preview::bit_cast<std::uint64_t>(f64v2), u64v2); // round-trip
 }
+
+TEST(VERSIONED(bit), byteswap_unsigned) {
+  constexpr std::uint8_t byte{ 0xCD };
+  EXPECT_EQ(preview::byteswap<std::uint8_t>(byte), byte);
+
+  constexpr std::uint16_t word{ 0xCA'FE };
+  EXPECT_EQ(preview::byteswap<std::uint16_t>(word), 0xFE'CA);
+  constexpr std::uint16_t word_same{ 0x44'44 };
+  EXPECT_EQ(preview::byteswap<std::uint16_t>(word_same), word_same);
+
+  constexpr std::uint32_t dword{ 0xDE'AD'BE'EF };
+  EXPECT_EQ(preview::byteswap<std::uint32_t>(dword), 0xEF'BE'AD'DE);
+  constexpr std::uint32_t dword_same{ 0x44'44'44'44 };
+  EXPECT_EQ(preview::byteswap<std::uint32_t>(dword_same), dword_same);
+
+  constexpr std::uint64_t qword{ 0x01'23'45'67'89'AB'CD'EF };
+  EXPECT_EQ(preview::byteswap<std::uint64_t>(qword), 0xEF'CD'AB'89'67'45'23'01);
+  constexpr std::uint64_t qword_same{ 0x44'44'44'44'44'44'44'44 };
+  EXPECT_EQ(preview::byteswap<std::uint64_t>(qword_same), qword_same);
+}
+
+TEST(VERSIONED(bit), byteswap_signed) {
+  constexpr std::int8_t byte{ 0x7D };
+  EXPECT_EQ(preview::byteswap<std::int8_t>(byte), byte);
+
+  constexpr std::int16_t word{ 0x7A'7E };
+  EXPECT_EQ(preview::byteswap<std::int16_t>(word), 0x7E'7A);
+  constexpr std::int16_t word_same{ 0x44'44 };
+  EXPECT_EQ(preview::byteswap<std::int16_t>(word_same), word_same);
+
+  constexpr std::int32_t dword{ 0x7E'AD'BE'7F };
+  EXPECT_EQ(preview::byteswap<std::int32_t>(dword), 0x7F'BE'AD'7E);
+  constexpr std::int32_t dword_same{ 0x44'44'44'44 };
+  EXPECT_EQ(preview::byteswap<std::int32_t>(dword_same), dword_same);
+
+  constexpr std::int64_t qword{ 0x01'23'45'67'89'AB'CD'44 };
+  EXPECT_EQ(preview::byteswap<std::int64_t>(qword), 0x44'CD'AB'89'67'45'23'01);
+  constexpr std::int64_t qword_same{ 0x44'44'44'44'44'44'44'44 };
+  EXPECT_EQ(preview::byteswap<std::int64_t>(qword_same), qword_same);
+}
