229e9b7f7187ae73bee25b91e76a17e12b16cc8b
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2024-10-09 01:47:38 +0200

    Replaced SFINAE by tag dispatching

--- include/preview/__bit/byteswap.h
+++ include/preview/__bit/byteswap.h
@@ -14,43 +14,38 @@
 #include "preview/__type_traits/negation.h"
 
 #if PREVIEW_CXX_VERSION >= 23
-    #include <bit>
+  #include <bit>
 #endif
 
 namespace preview {
 namespace detail {
 
-template<typename T, std::enable_if_t<std::is_unsigned<T>::value, int> = 0>
-constexpr T byteswap_impl(T n) noexcept {
-    constexpr T byte_mask{ (1 << CHAR_BIT) - 1 };
+template<typename T>
+constexpr T byteswap_impl(T n, std::true_type /* is unsigned */) noexcept {
+  constexpr T byte_mask{ (1 << CHAR_BIT) - 1 };
+  T result{ 0 };
 
-    T result{ 0 };
-    for (unsigned i{ 0 }; i < sizeof(T); i++) {
-        const std::size_t nth_byte{ CHAR_BIT * (sizeof(T) - i - 1) };
-        const T byte{ static_cast<T>((n & (byte_mask << nth_byte)) >> nth_byte) };
-        result |= byte << (i * CHAR_BIT);
-    }
-    return result;
+  for (unsigned i{ 0 }; i < sizeof(T); i++) {
+    const std::size_t nth_byte{ CHAR_BIT * (sizeof(T) - i - 1) };
+    const T byte{ static_cast<T>((n & (byte_mask << nth_byte)) >> nth_byte) };
+    result |= byte << (i * CHAR_BIT);
+  }
+  return result;
 }
 
-template<typename T, std::enable_if_t<std::is_signed<T>::value, int> = 0, typename UnsignedT = typename std::make_unsigned<T>::type>
-constexpr UnsignedT byteswap_impl(T n) {
-    return byteswap_impl<UnsignedT>(preview::bit_cast<UnsignedT>(n));
-}
-
-template<typename T, std::enable_if_t<preview::negation_v<std::is_integral<T>>, int> = 0>
-constexpr T byteswap_impl(T) {
-    static_assert(false, "byteswap needs integral type !");
+template<typename T, typename UnsignedT = typename std::make_unsigned<T>::type>
+constexpr UnsignedT byteswap_impl(T n, std::false_type /* is signed */) {
+  return preview::bit_cast<UnsignedT>(preview::detail::byteswap_impl<UnsignedT>(preview::bit_cast<UnsignedT>(n), std::true_type{}));
 }
 
 } // namespace detail
 
-template<typename T>
-constexpr T byteswap(T n) noexcept {
+template<typename T, std::enable_if_t<std::is_integral<T>::value, int> = 0>
+constexpr inline T byteswap(T n) noexcept {
 #if PREVIEW_CXX_VERSION >= 23
-    return std::byteswap<T>(n);
+  return std::byteswap<T>(n);
 #else
-    return preview::detail::byteswap_impl<T>(n);
+  return sizeof(T) == 1 ? n : preview::detail::byteswap_impl<T>(n, std::is_unsigned<T>{});
 #endif
 }
 
