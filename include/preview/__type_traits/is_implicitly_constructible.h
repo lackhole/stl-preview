05b8328b06d9b725630eb09b8eb0f52293caf7cc
Yong Gyu Lee <cosgenio@gmail.com>
2024-10-27 22:25:39 +0900

    Update is_implicitly_constructible.h

--- include/preview/__type_traits/is_implicitly_constructible.h
+++ include/preview/__type_traits/is_implicitly_constructible.h
@@ -9,16 +9,26 @@
 
 #include "preview/core.h"
 #include "preview/__type_traits/conjunction.h"
+#include "preview/__type_traits/is_implicitly_default_constructible.h"
 #include "preview/__type_traits/negation.h"
 
 namespace preview {
+namespace detail {
 
 template<typename T, typename... Args>
-struct is_implicitly_constructible :
+struct is_implicitly_constructible_impl :
     conjunction<
-      std::is_constructible<T, Args...>,
-      std::is_convertible<Args..., T>
+        std::is_constructible<T, Args...>,
+        std::is_convertible<Args..., T>
     > {};
+
+template<typename T>
+struct is_implicitly_constructible_impl<T> : is_implicitly_default_constructible<T> {};
+
+} // namespace detail
+
+template<typename T, typename... Args>
+struct is_implicitly_constructible : detail::is_implicitly_constructible_impl<T, Args...> {};
 
 template<typename T, typename... Args>
 PREVIEW_INLINE_VARIABLE constexpr bool is_implicitly_constructible_v = is_implicitly_constructible<T, Args...>::value;
