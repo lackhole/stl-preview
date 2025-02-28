4975f7b7be06c3148dfeaed70c50cb99d8729c81
Yong Gyu Lee <cosgenio@gmail.com>
2025-02-28 22:48:25 +0900

    Create PreviewTest.cmake

---
+++ cmake/PreviewTest.cmake
@@ -0,0 +1,35 @@
+include(FindGoogleTest)
+
+function(preview_add_test name source)
+    add_executable(${name} "${source}")
+    target_link_libraries(${name} GTest::gtest_main)
+    add_test(NAME ${name} COMMAND ${name})
+
+    if (NOT ANDROID)
+        gtest_discover_tests(${name} DISCOVERY_TIMEOUT 60)
+    endif ()
+
+    if (ANDROID)
+        #        add_custom_command(TARGET ${name} POST_BUILD
+        #            COMMAND ${ADB} shell mkdir -p /data/local/tmp/${ANDROID_ABI}
+        #            COMMAND ${ADB} push $<TARGET_FILE:${name}> /data/local/tmp/${ANDROID_ABI}/
+        #            COMMAND ${ADB} shell \"export LD_LIBRARY_PATH=/data/local/tmp/${ANDROID_ABI}\; /data/local/tmp/${ANDROID_ABI}/${name}\")
+    elseif (EMSCRIPTEN)
+        target_link_libraries(${name} "-sEXIT_RUNTIME=1 -sALLOW_MEMORY_GROWTH=1 -Wno-pthreads-mem-growth -fexceptions")
+        target_compile_options(${name}
+            PUBLIC "-fexceptions")
+    elseif (MSVC)
+        target_compile_options(${name}
+            PRIVATE "/bigobj"
+            PRIVATE "/WX")
+        target_compile_definitions(${name}
+            PRIVATE _CRT_SECURE_NO_WARNINGS)
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        target_compile_options(${name}
+            PRIVATE "-Wall"
+            PRIVATE "-Werror"
+            PRIVATE "-Wno-unused-local-typedef"
+            PRIVATE "-Wno-unused-variable"
+        )
+    endif ()
+endfunction()
