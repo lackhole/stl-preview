024b87eda81de69f1d22a1672c6b132d726bf7b1
Yong Gyu Lee <cosgenio@gmail.com>
2025-04-28 17:46:41 +0900

    Add test

--- test/ranges.cc
+++ test/ranges.cc
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <array>
 #include <cstring>
+#include <deque>
 #include <list>
 #include <vector>
 
@@ -470,4 +471,23 @@ TEST(VERSIONED(Ranges), iterator_conforming) {
   for (auto c : views::concat(v, s, sv, l)) {
     std::cout << c;
   }
+}
+
+TEST(VERSIONED(Ranges), reserve_hint) {
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>{}), 0);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10)), 10);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::as_const), 10);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::take(5)), 5);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::take(15)), 10);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::drop(4)), 6);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::drop(15)), 0);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::take(5) | views::enumerate), 5);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::reverse), 10);
+  EXPECT_EQ(ranges::reserve_hint(std::vector<int>(10) | views::reverse | views::transform(preview::identity{})), 10);
+
+  EXPECT_EQ(ranges::reserve_hint(std::list<int>{}), 0);
+  EXPECT_EQ(ranges::reserve_hint(std::list<int>(10)), 10);
+
+  EXPECT_EQ(ranges::reserve_hint(std::deque<int>{}), 0);
+  EXPECT_EQ(ranges::reserve_hint(std::map<int, int>{{1, 2}}), 1);
 }
